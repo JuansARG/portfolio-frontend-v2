d29f0b66fdc0e3af8be2c7e7338b5071531164b0
Juan Sarmiento <juansarmientog96@gmail.com>
2023-08-22 19:40:10 -0300

    clean backendApi.ts

--- src/api/BackendApi.ts
+++ src/api/BackendApi.ts
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 const backendApi = axios.create({
     baseURL: 'http://localhost:8080',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    // withCredentials: true
 });
 
 export default backendApi;
