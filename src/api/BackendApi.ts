7378e68221a2b0a525dec87b3c1b949faff23161
Juan Sarmiento <juansarmientog96@gmail.com>
2023-09-23 18:00:52 -0300

    modify url backend api

--- src/api/BackendApi.ts
+++ src/api/BackendApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const backendApi = axios.create({
-    baseURL: import.meta.env.VITE_URL_BACKEND_PROD,
+    baseURL: import.meta.env.VITE_URL_BACKEND_DEV,
 });
 
 export default backendApi;
